02b05470420f9c0c8b02a941303d635b8ee25d72
Aliza khan <155885247+alizak-13@users.noreply.github.com>
2024-05-26 09:53:32 -0700

    Add files via upload
    A simple calculator using inquire and node.js

---
+++ main.ts
@@ -0,0 +1,23 @@
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  { message: "Enter firstnum", type: "number", name: "firstnum" },
+  { message: "Enter secondnum", type: "number" , name: "secondnum" },
+  {
+    message: "Please select operator to perform operation",
+    type: "list",
+    name: "operator",
+    choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+  },
+]);
+if (answer.operator === "Addition") {
+  console.log(answer.firstnum + answer.secondnum);
+} else if (answer.operator === "Subtraction") {
+  console.log(answer.firstnum - answer.secondnum)
+} else if (answer.operator === "Multiplication") {
+  console.log(answer.firstnum * answer.secondnum)
+} else if (answer.operator === "Division") {
+  console.log(answer.firstnum / answer.secondnum)
+} else {
+  console.log ("Please select a valid operator")
+}
